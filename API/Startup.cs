ddc98ebeebff3a40b94283f6ad554567357211f6
Jaime Alvarez <jaimeandresalvarez@gmail.com>
2022-07-03 18:34:26 -0500

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,6 +1,7 @@
 using Infrastructure.Data;
 using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
+using Core.Interfaces;
 
 namespace API
 {
@@ -15,7 +16,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
             services.AddSwaggerGen(c =>
